1b4b471aacb237850886df5d3eaf7058b6213f5f
diriho <don_destin_iriho@brown.edu>
2026-07-15 15:33:40 -0400

    feat: initial setu p

--- server/config/reset.js
+++ server/config/reset.js
@@ -26,15 +26,41 @@ const createTripsTable = async () => {
       );
   `
   try {
-  const res = await pool.query(createTripsTableQuery)
+  const res = 
+  await pool.query(createTripsTableQuery)
   console.log('🎉 trips table created successfully')
     } catch (err) {
         console.error('⚠️ error creating trips table', err)
     }
 }
 
+
 const seedTripsTable = async () => {
   await createTripsTable()
+  tripsData.forEach(async (trip) => {
+    const insertQuery = {
+        text: 'INSERT INTO trips (title, description, img_url, num_days, start_date, end_date, total_cost) VALUES ($1, $2, $3, $4, $5, $6, $7)'
+    }
+    const values = [
+        trip.title,
+        trip.description,
+        trip.img_url,
+        trip.num_days,
+        trip.start_date,
+        trip.end_date,
+        trip.total_cost
+    ]
+    pool.query(insertQuery, values, (err, res) => {
+    if (err) {
+        console.error('⚠️ error inserting trip', err)
+        return
+    }
+
+    console.log(`✅ ${trip.title} added successfully`)
+})
+
+  })
 }
 
+seedTripsTable()
 
