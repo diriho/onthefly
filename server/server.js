6a7ed203fa00aa0444a1da2182ca35e0b4889759
diriho <don_destin_iriho@brown.edu>
2026-07-22 18:38:07 -0400

    client side face interface commit

--- server/server.js
+++ server/server.js
@@ -16,15 +16,16 @@ app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
 })
 
+// define the routes
 app.use('/api/trips', tripRoutes)
 app.use('/api/activities', activityRoutes)
 app.use('/api/destinations', destinationRoutes)
 app.use('/api/trips_destinations', tripDestinationsRoutes)
 
 
-
 const PORT = process.env.PORT
 
+// start the port
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 })
